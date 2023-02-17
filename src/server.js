7a996cfe17db44fbbbe4c2dcf40cb6b1c3d88ab0
Simão Neto <simaomenezesneto@gmail.com>
2023-02-17 17:51:19 +0000

    feat: Create middleware, create route POST and GET.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,36 @@
+import { randomUUID } from "node:crypto"
 import http from "node:http"
-const server = http.createServer((req, res) => {
+import { json } from "./middlewares/json.js"
 
-    return res.end("OK!")
+const tasks = []
+const server = http.createServer(async (req, res) => {
+    res.setHeader('Content-type', 'application/json')
+    const { method, url } = req
+
+    await json(req, res)
+
+    console.log(req.body)
+
+    if(method === "POST" && url === "/tasks") {
+
+        const { title, decription } = req.body
+
+        const task = {
+            id: randomUUID(),
+            title,
+            decription,
+            created_at: new Date()
+        }
+        tasks.push(task)
+
+        return res.writeHead(201).end()
+    }
+
+    if(method === "GET" && url === "/tasks") {        
+        return res.writeHead(200).end(JSON.stringify(tasks))
+    }
+
+    return res.end("sdsd")
 
 })
 
